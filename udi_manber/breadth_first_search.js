2ce19c4f40189e30841f8cfdb822feb5c20cf955
TrongHai <dotronghai96@gmail.com>
2018-08-09 16:57:29 +0700

    Implement Breadth-first Search algorithm

---
+++ udi_manber/breadth_first_search.js
@@ -0,0 +1,28 @@
+const Graph = require('./graph.js')
+
+let graph = new Graph("graph.txt").getAdjacencyList()
+
+function breadth_first_search(graph) {
+    let first_element = graph.filter(eles => eles.length > 0)[0][0]
+    let current_element
+
+    let undiscovered = Array(graph.length).fill(false)
+    let parent = Array(graph.length).fill(null) 
+    let queue = new Array()
+
+    undiscovered[first_element] = true
+    queue.push(first_element)
+
+    while (queue.length > 0) {
+        current_element = queue.shift()
+        console.log(current_element)
+        for (let adjacent_vertex of graph[current_element]) {
+            if (!undiscovered[adjacent_vertex]) {
+                undiscovered[adjacent_vertex] = true
+                parent[adjacent_vertex] = current_element
+                queue.push(adjacent_vertex)
+            }
+        }
+    }
+}
+
