0c5cb3f73738ade8a6b4a07702c41dce92b90d8e
TrongHai <dotronghai96@gmail.com>
2018-08-10 12:34:57 +0700

    Comment and modify BFS to process inputs instead of creating side-effects

--- udi_manber/breadth_first_search.js
+++ udi_manber/breadth_first_search.js
@@ -2,12 +2,12 @@ const Graph = require(`${__dirname}/graph.js`)
 
 let graph = new Graph(`${__dirname}/graph.txt`).getAdjacencyList()
 
-function breadth_first_search(graph) {
-    let first_element = graph.filter(eles => eles.length > 0)[0][0]
+function breadth_first_search(graph, input_element, input_parent, input_undiscovered) {
+    let first_element = (input_element) ? input_element : graph.filter(eles => eles.length > 0)[0][0]
     let current_element
 
-    let undiscovered = Array(graph.length).fill(false)
-    let parent = Array(graph.length).fill(null) 
+    let undiscovered = (input_undiscovered) ? input_undiscovered : Array(graph.length).fill(false)
+    let parent = (input_parent) ? input_parent : Array(graph.length).fill(null) 
     let queue = new Array()
 
     undiscovered[first_element] = true
@@ -15,7 +15,7 @@ function breadth_first_search(graph) {
 
     while (queue.length > 0) {
         current_element = queue.shift()
-        console.log(current_element)
+        console.log(`current_element=${current_element}`)
         for (let adjacent_vertex of graph[current_element]) {
             if (!undiscovered[adjacent_vertex]) {
                 undiscovered[adjacent_vertex] = true
@@ -24,7 +24,7 @@ function breadth_first_search(graph) {
             }
         }
     }
-    return parent 
+    return { parent, undiscovered }
 }
 
 function finding_path(start, end, parent) {
@@ -38,4 +38,22 @@ function finding_path(start, end, parent) {
     return passing_nodes.reverse().join('->')
 }
 
-let parent = breadth_first_search(graph)
+function connected_components(graph) {
+    console.log(`graph.length=${graph[6]}`)
+    let parent = Array(graph.length).fill(null)
+    let undiscovered = Array(graph.length).fill(false)
+    let undiscovered_indices = [...Array(graph.length).keys()].splice(1)
+    console.log(`undiscovered_indices=${undiscovered_indices}`)
+    console.log(`undiscored=${undiscovered}`)
+    let components = 0
+    for (let index of undiscovered_indices) {
+        if (!undiscovered[index]) {
+            console.log(undiscovered)
+            components += 1;
+            ({parent, undiscovered} = breadth_first_search(graph, index, parent, undiscovered));
+        }
+    }
+    return components
+}
+
+console.log(`Connected components of graph is ${connected_components(graph)}`)
